90223466c9fe6c94b9fc1edcebe76045455de512
Abdlrrahman <abdlrrahman.shibani@gmail.com>
2020-01-07 16:02:44 +0100

    connected to the db

--- model/category.js
+++ model/category.js
@@ -1,4 +1,17 @@
 const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/store', { useNewUrlParser: true });
+
+let db = mongoose.connection
+
+db.on('error', function () {
+    console.log('mongoose connection error');
+});
+
+db.once('open', function () {
+    console.log('mongoose connected successfully');
+});
+
 const Schema = mongoose.Schema({
     name: String
 });
